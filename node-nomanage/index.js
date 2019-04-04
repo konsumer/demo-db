e6992c4e329d64fda27160fa0d7ed3e934ade6c4
David Konsumer <konsumer@jetboystudio.com>
2019-04-04 15:40:41 -0700

    a vanilla sqlite example, and nowignore

---
+++ node-nomanage/index.js
@@ -0,0 +1,15 @@
+const { Database } = require('sqlite3')
+
+const db = new Database(process.env.DATABASE_URI.replace(/^sqlite:\/\//, ''))
+
+const query = q => new Promise((resolve, reject) => db.all(q, (err, rows) => err ? reject(err) : resolve(rows)))
+
+const run = async () => {
+  const r = await query('SELECT 1+1 AS result')
+  console.log(r[0].result)
+}
+run()
+
+module.exports = (req, res) => {
+  res.end(`Hello from node. Your database is at ${process.env.DATABASE_URI}. I tried to touch it.`)
+}
